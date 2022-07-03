c7bf204ab1bb058c02033d75d1003a2fea124e86
feyerina <feyerina@mail.ru>
2022-07-03 14:11:46 +0300

    Using load model

--- app.py
+++ app.py
@@ -1,21 +1,38 @@
 import asyncio
+import logging
+from aiogram import executor
+from aiogram.utils.executor import start_webhook
+from data.config import WEBHOOK_URL, WEBHOOK_PATH, WEBAPP_HOST, WEBAPP_PORT
+from handlers import dp
+from loader import  bot
 
-async def on_startup(dp):
 
+
+async def on_startup(dispatcher):
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
 
     from utils.notify_admins import on_startup_notify
-    await on_startup_notify(dp)
-
+    await on_startup_notify(dispatcher)
 
     from utils.set_bot_commands import set_default_commands
-    await set_default_commands(dp)
+    await set_default_commands(dispatcher)
 
     print('Bot works!')
 
 
+async def on_shutdown(dispatcher):
+    await bot.delete_webhook()
+
+
 if __name__ == '__main__':
-    from aiogram import executor
-    from handlers import dp
-
-
-    executor.start_polling(dp, on_startup=on_startup)
+    logging.basicConfig(level=logging.INFO)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
+    #executor.start_polling(dp, on_startup=on_startapp)
--- data/config.py
+++ data/config.py
@@ -5,7 +5,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = str(os.getenv("BOT_TOKEN"))
+HEROKU_APP_NAME = str(os.getenv("HEROKU_APP_NAME"))
 
 admins_id = [
     567790058
 ]
+
+# webhook settings
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_PATH = f'/webhook/{BOT_TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
+# webserver settings
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = int(os.getenv('PORT', default=8000))
